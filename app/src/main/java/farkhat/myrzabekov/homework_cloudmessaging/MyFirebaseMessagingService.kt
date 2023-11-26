785dcd6ef202e3f213b9fabbe4d9916d821a4436
Farkhat <farhad020202@gmail.com>
2023-11-26 21:50:42 +0600

    Initial commit

---
+++ app/src/main/java/farkhat/myrzabekov/homework_cloudmessaging/MyFirebaseMessagingService.kt
@@ -0,0 +1,78 @@
+package farkhat.myrzabekov.homework_cloudmessaging
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.data.isNotEmpty()) {
+            handleDataMessage(remoteMessage.data)
+        }
+
+        remoteMessage.notification?.let {
+            handleNotification(it)
+        }
+    }
+
+    private fun handleDataMessage(data: Map<String, String>) {
+        val myValue = data["myKey"]
+
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(applicationContext, myValue, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun handleNotification(notification: RemoteMessage.Notification) {
+        val title = notification.title ?: "Default title"
+        val messageBody = notification.body ?: "Default message"
+
+        sendNotification(title, messageBody)
+    }
+
+    private fun sendNotification(title: String, messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_rocket)
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
+
